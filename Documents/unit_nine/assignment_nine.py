4349d6c13a382098477c99bed138dd119ec2891d
Thenea <thenea@iMac4.local>
2018-11-30 11:18:28 -0500

    This is my first eidition of the program, and I put in draw new cards and comparation

--- Documents/unit_nine/assignment_nine.py
+++ Documents/unit_nine/assignment_nine.py
@@ -2,48 +2,40 @@
 # Nov. 29th
 # This is a program about a game which designs a simplified game of Baccarat. The game is easy, but the program is hard.
 
-import card
+import deck
 import random
 
 
-class Deck:
-
-    def __int__(self):
-
-        self.deck_of_cards = []
-        for rank in range(13):
-            for suit in range(4):
-                new_card = card.Card(rank, suit)
-                self.deck_of_cards.append(new_card)
-
-    def draw_a_card(self):
-        new_card = self.deck_of_cards.pop()
-        return new_card
-
-    def shuffle(self):
-        random.shuffle(self.deck_of_cards)
-
-
-class Card:
-
-    def __init__(self, rank, suit):
-
-        self.rank = rank
-        self.suit = suit
-
-    def compared_to(self, other_card):
-        if self.rank > other_card.rank:
-            return self
-        elif other_card.rank > self.rank:
-            return other_card
+def main():
+    deck1 = deck.Deck()
+    deck1.shuffle()
+    card1 = deck1.draw_a_card()
+    card2 = deck1.draw_a_card()
+    card3 = deck1.draw_a_card()
+    card4 = deck1.draw_a_card()
+    print("You got a", card1, "and a", card2)
+    number1 = card1.rank + 1 + card2.rank + 1
+    number2 = card3.rank + 1 + card4.rank + 1
+    if number1 >= 10:
+        number3 = number1 - 10
+        print("Your number is", number3)
+    else:
+        print("Your number is", number1)
+    print("The dealer got a", card3, "and a", card4)
+    if number2 >= 10:
+        number4 = number2 - 10
+        print("The dealer's number is", number4)
+    else:
+        print("The dealer's number is", number2)
+    if number2 == 9 or number2 == 8 or number1 == 8 or number1 == 9:
+        if number1 > number2:
+            print("Nice! You win this one!")
+        elif number1 == number2:
+            print("This is a tie!")
         else:
-            if self.suit > other_card.suit:
-                return self
-            else:
-                return other_card
+            print("I am sorry, The dealer wins this one")
+    else:
+        pass
 
-    def __str__(self):
-        ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Right", "Nine", "Ten", "Jack", "Queen", "King"]
-        suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
-        new_card = ranks[self.rank] + "of" + suits[self.suit]
-        return new_card
+
+main()
