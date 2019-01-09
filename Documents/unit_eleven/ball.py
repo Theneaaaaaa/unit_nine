b0a28429c936f33a183fc45a6c949081787bc61d
Thenea <thenea@iMac4.local>
2019-01-09 14:40:51 -0500

    new setups

--- Documents/unit_eleven/ball.py
+++ Documents/unit_eleven/ball.py
@@ -4,15 +4,17 @@ class Ball(pygame.sprite.Sprite):
 
     def __init__(self, color, windowWidth, windowHeight, radius):
         # initialize sprite super class
-
+        super().__init__()
         # finish setting the class variables to the parameters
         self.color = color
-
+        self.radius = radius
         # Create a surface, get the rect coordinates, fill the surface with a white color (or whatever color the
         # background of your breakout game will be.
-        self.image = pygame.Surface((self.width, self.height))
+        self.image = pygame.Surface((self.radius, self.radius))
         self.rect = self.image.get_rect()
-        self.windowWidth =
+        self.windowWidth = windowWidth
+        self.windowHeight = windowHeight
+
 
 
         # Add a circle to represent the ball to the surface just created.
