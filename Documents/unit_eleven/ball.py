664960d52c9485a749584cc801572430686dbf21
Thenea <thenea@iMac4.local>
2019-01-09 14:38:31 -0500

    trying to make the ball work

---
+++ Documents/unit_eleven/ball.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Ball(pygame.sprite.Sprite):
+
+    def __init__(self, color, windowWidth, windowHeight, radius):
+        # initialize sprite super class
+
+        # finish setting the class variables to the parameters
+        self.color = color
+
+        # Create a surface, get the rect coordinates, fill the surface with a white color (or whatever color the
+        # background of your breakout game will be.
+        self.image = pygame.Surface((self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.windowWidth =
+
+
+        # Add a circle to represent the ball to the surface just created.
+
+    def move(self):
+        pass
