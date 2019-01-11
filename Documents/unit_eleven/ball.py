71724c775924c8fb0f2db3e84cac13b3f184885a
Thenea <thenea@iMac4.local>
2019-01-11 08:59:24 -0500

    set up buncing off the wall and the paddle, and collide settings

--- Documents/unit_eleven/ball.py
+++ Documents/unit_eleven/ball.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ball(pygame.sprite.Sprite):
 
     def __init__(self, color, windowWidth, windowHeight, radius):
@@ -14,10 +15,28 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.windowWidth = windowWidth
         self.windowHeight = windowHeight
-
-
-
         # Add a circle to represent the ball to the surface just created.
+        self.speed_x = 7
+        self.speed_y = 6
 
     def move(self):
-        pass
+        self.rect.x = self.rect.x + self.speed_x
+        self.rect.y = self.rect.y + self.speed_y
+        if self.rect.right >= self.windowWidth:
+            self.speed_x = -self.speed_x
+        elif self.rect.left <= 0:
+            self.speed_x = -self.speed_x
+        elif self.rect.top <= 0:
+            self.speed_y = -self.speed_y
+            self.speed_x = -self.speed_x
+
+    def collide(self, spriteGroup):
+        if pygame.sprite.spritecollide(self, spriteGroup, False):
+            self.speed_x = -self.speed_x
+            self.speed_y = -self.speed_y
+
+    def collideBrick(self, spriteGroup):
+        if pygame.sprite.spritecollide(self, spriteGroup, True):
+            self.speed_x = -self.speed_x
+            self.speed_y = -self.speed_y
+
